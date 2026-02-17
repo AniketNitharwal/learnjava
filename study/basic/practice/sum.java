398b1727d20c5fd36d3c4cdde83da9bb61e1e218
AniketNitharwal <2405332@kiit.ac.in>
2026-02-17 20:36:28 +0530

    learned use of scanner

--- study/basic/practice/sum.java
+++ study/basic/practice/sum.java
@@ -2,7 +2,10 @@ class sum{
     public static void main(String args []) {
         int a = 20;
         int b = 25;
+        a=25;
         int sum=a+b;
         System.out.println("sum ="+sum);
+        double cal = (a * b)/(sum);
+        System.out.println("cal ="+cal);
     }
 }
